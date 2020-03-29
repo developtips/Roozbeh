70a08d883deb5f4f3665b8af3c478d755db2d447
vahid <Shahbazian@live.com>
2020-03-29 03:38:15 -0700

    initial

---
+++ FrontEnd/assets/lib/horizontal-text-scrolling/dist/js/model.js
@@ -0,0 +1,18 @@
+﻿var newsTickerJSON = {
+
+    "one": {
+        "headline": "تست 1 ..."
+    },
+
+    "two": {
+        "headline": "تست 2 ..."
+    },
+
+    "three": {
+        "headline": "تست 3 ..."
+        //"url": "https://www.jqueryscript.net"
+    },
+
+    // more data here
+
+}
